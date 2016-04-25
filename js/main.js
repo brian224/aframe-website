a1420a0d13e0695919ac3072b891075801e043df
yifan <yifan@yifans-iMac.local>
2016-04-25 18:00:28 +0800

    Working on layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var newsEntities = Array.prototype.slice.call(document.querySelectorAll('#news-entities a-entity'));
+var newsEntities = Array.prototype.slice.call(document.querySelectorAll('.news-entity-container'));
 newsEntities.forEach((entity, i) => {
   console.log(entity, i);
-  entity.object3D.rotation.x = Math.PI/30 * (1 - i);
+  entity.object3D.rotation.x = Math.PI/12 * (1 - i);
 });
