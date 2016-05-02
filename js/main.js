f017f6e58dd1776a73f4b4a35e82a65f6cdcc27a
Yi-Fan Liao <begeeben@gmail.com>
2016-05-02 17:25:35 +0800

    Draft layout and focus animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-var newsEntities = Array.prototype.slice.call(document.querySelectorAll('.news-entity-container'));
-newsEntities.forEach((entity, i) => {
-  console.log(entity, i);
-  entity.object3D.rotation.x = Math.PI/12 * (1 - i);
+// var newsEntities = Array.prototype.slice.call(document.querySelectorAll('.news-entity-container'));
+// newsEntities.forEach((entity, i) => {
+//   console.log(entity, i);
+//   entity.object3D.rotation.x = Math.PI/12 * (1 - i);
+// });
+
+AFRAME.registerComponent('click-listener', {
+  init: function () {
+    var el = this.el;
+    window.addEventListener('click', function () {
+      el.emit('click', null, false);
+    });
+  }
 });
